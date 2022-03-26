4ec822440906b22801b962d38062a5aaecb92de4
Anietie Brownson <anni.brownson@gmail.com>
2022-03-26 21:41:15 +0100

    Added images

--- src/Components/Pages/MainPage.elements.js
+++ src/Components/Pages/MainPage.elements.js
@@ -3,13 +3,68 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.div`
 display: flex;
+flex-direction: row;
+width: 100%;
 `
 
 
 export const PicturesContainer = styled.div`
-display: block;
+display: flex;
+margin-top: 5%;
+width: 50%;
+height: 50%;
+
+& > img {
+    width: 100%;
+    border-radius: 15px;
+    height: 20%;
+}
 `
 
 export const InfoContainer = styled.div`
 display: flex
+`
+
+export const ThumbNails = styled.div`
+display: flex;
+flex-direction: row;
+`
+
+export const Thumbs = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: 35%;
+margin-inline-start: 10%;
+cursor: pointer;
+
+& > img {
+    margin-top: 5%;
+    width: 20%;
+    height: 5%;
+    border-radius: 10px;
+}
+
+& > img:hover {
+    opacity: 0.5;
+}
+
+@media screen and (max-width: 768px) {
+    display: none;
+}
+`
+
+export const CompanyName = styled.div`
+text-transform: uppercase;
+color: hsl(26, 100%, 50%);
+margin-top: 5%;
+font-size: smaller;
+`
+
+export const ProductInfo = styled.div`
+font-size: 15px;
+`
+
+export const ProductPriceBig = styled.div`
+font-size: 25px;
+font-weight: 900;
 `
--- src/Components/Pages/MainPage.js
+++ src/Components/Pages/MainPage.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { MainContainer, PicturesContainer, InfoContainer } from './MainPage.elements'
+import { MainContainer, PicturesContainer, InfoContainer, ThumbNails, Thumbs, 
+  CompanyName, ProductInfo, ProductPriceBig} from './MainPage.elements'
 import Product from '../../Assets/images/image-product-1.jpg'
+import Thumbnail from '../ProductThumbnails/Thumbnail.json'
+
 
 const MainPage = () => {
   return (
@@ -8,9 +11,29 @@ const MainPage = () => {
         <MainContainer>
             <PicturesContainer>
               <img src={Product} alt='sneaker snapshot' />
+              <ThumbNails>
+                {
+                  Thumbnail && Thumbnail.map((thumbs) => {
+                    return (
+                      <Thumbs key={thumbs.id}>
+                        <img src={thumbs.image} alt={thumbs.alt} />
+                      </Thumbs>
+                    )
+                  })
+                }
+              </ThumbNails>
             </PicturesContainer>
-            <InfoContainer>
 
+            <InfoContainer>
+              <CompanyName>
+                Sneaker Company
+              </CompanyName>
+              <ProductInfo>
+                Fall Limited Edition Sneakers
+              </ProductInfo>
+              <ProductPriceBig>
+
+              </ProductPriceBig>
             </InfoContainer>
         </MainContainer>
     </>
