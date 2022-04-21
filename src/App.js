2a5f691104b7e603db59ba60f77ce0b34cdbe887
Anietie Brownson <anni.brownson@gmail.com>
2022-04-21 01:37:38 +0100

    minor additions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import MainPage from './Components/Pages/MainPage';
-import GlobalStyles from './GlobalStyles'
+
 
 
 export default function App() {
   return (
     <>
-      <GlobalStyles />
-      <Navbar />
-      <MainPage />
+      
     </>
   );
 }
