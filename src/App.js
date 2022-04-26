4059e56b25a37457f9826ab1866f802d1a2542d5
Anietie Brownson <anni.brownson@gmail.com>
2022-04-26 19:56:17 +0100

    file changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import About from './Components/Pages/About';
+import Home from "./Components/Pages/Home";
+
+
+export default function App() {
+  return (
+    <>
+      <About />
+      <Home />
+    </>
+  );
+}
